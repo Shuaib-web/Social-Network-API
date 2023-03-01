9b8dafb0e59a9d96681b0d1c590048495a622eed
shuib <shuaib_97@hotmail.co.uk>
2023-03-01 19:39:56 +0000

    set up get by id for both user and thoughts, as well as create for both

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -14,8 +14,48 @@ const thoughtController = {
         res.status(500).json(err);
       });
   },
-  getThoughtById(req, res) {},
-  createThought(req, res) {},
+  getThoughtById({ params }, res) {
+    Thought.findOne({ _id: params.id })
+      .populate({
+        path: "reactions",
+        select: "-__v",
+      })
+      .select("-__v")
+      .then((getThoughtByIdData) => {
+        if (!getThoughtByIdData) {
+          res
+            .status(404)
+            .json({ message: "No Thought with this Id was found!" });
+          return;
+        }
+        res.json(getThoughtByIdData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+  createThought({ body }, res) {
+    Thought.create(body)
+      .then((_id) => {
+        return User.findOneAndUpdate(
+          { _id: body.userId },
+          { $push: { thoughts: _id } },
+          { new: true }
+        );
+      })
+      .then((createThoughtData) => {
+        if (!createThoughtData) {
+          res.status(404).json({ message: "No User found with this Id!" });
+          return;
+        }
+        res.json(createThoughtData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
   updateThought(req, res) {},
   deleteThought(req, res) {},
   addReaction(req, res) {},
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -15,7 +15,26 @@ const userController = {
         res.status(500).json(err);
       });
   },
-  getUsersById(req, res) {},
+  getUsersById({ params }, res) {
+    User.findOne({ _id: params.id })
+      .populate({
+        path: "thoughts",
+        select: "-__v",
+      })
+      .select("-__v")
+      .sort({ _id: -1 })
+      .then((getUserByIdData) => {
+        if (!getUserByIdData) {
+          res.status(404).json({ message: "No user with this id was found!" });
+          return;
+        }
+        res.json(getUserByIdData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
   createUser({ body }, res) {
     User.create(body)
       .then((createUserData) => res.json(createUserData))
