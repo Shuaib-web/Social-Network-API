2498c8a123642d5d053dd10bf5a70ed8a6675fa0
shuib <shuaib_97@hotmail.co.uk>
2023-03-08 20:05:58 +0000

    set up add and delete reaction

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -86,8 +86,33 @@ const thoughtController = {
         res.status(500).json(err);
       });
   },
-  addReaction(req, res) {},
-  deleteReaction(req, res) {},
+  addReaction({ params, body }, res) {
+    Thought.findOneAndUpdate(
+      { _id: params.thoughtId },
+      { $addToSet: { reactions: body } },
+      { new: true }
+    )
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          res.status(404).json({ message: "No Thought found with this id!" });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch((err) => res.json(err));
+  },
+
+  deleteReaction({ params }, res) {
+    Thought.findOneAndUpdate(
+      { _id: params.thoughtId },
+      { $pull: { reactions: { reactionId: params.reactionId } } },
+      { new: true }
+    )
+      .then((dbThoughtData) => {
+        res.json(dbThoughtData);
+      })
+      .catch((err) => res.json(err));
+  },
 };
 
 module.exports = thoughtController;
